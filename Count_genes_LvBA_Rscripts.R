79334a2ccfe2e01dfc1718b599434f843c448688
Lars Andersen <larsmew@gmail.com>
2021-08-20 12:19:08 +0200

    Added explanatory comments
    - Provided more explanatory comments
    - Reorganized code to fit 80 characters per line

--- Count_genes_LvBA_Rscripts.R
+++ Count_genes_LvBA_Rscripts.R
@@ -1,52 +1,72 @@
 library(readxl)
 library(dplyr)
-library(ggplot2)
 
+# List Excel files with all variants from the 29 families
+#  - Output from Varseq with relevant columns selected
 files = list.files(pattern="*-simple.xlsx")
 files
 
-#####----FILE WITH THE ALL VARIANTS FROM 29 FAMILIES
-
+# Function to read Excel files
 read_files <- function(x){
+  # Extract family name from file name
   fam_name = strsplit(strsplit(x, "-")[[1]][1], " ")[[1]][1]
+  # Read Excel file, skipping the first line, and selecting the first 20 columns
   dat <- read_excel(x, col_names = F, skip = 1)[, 1:20 ]
-  colnames(dat) <- c("pos","ref/alt","gene","seq_ontology","effect","Transcript", "HGVS_c","HGVS_p", "AF", "Alt_allele_count", 
-                     "Homozygote_count", "Hemizygote_count", "Prediction", "PHRED", "HGMD", 
-                     "Disorder", "Inheritance", "Clinical_significance", "Aggregate", "Pho")
+  # naming the columns
+  colnames(dat) <- c("pos","ref/alt","gene","seq_ontology","effect",
+                     "Transcript", "HGVS_c","HGVS_p", "AF", "Alt_allele_count",
+                     "Homozygote_count", "Hemizygote_count", "Prediction",
+                     "PHRED", "HGMD", "Disorder", "Inheritance",
+                     "Clinical_significance", "Aggregate", "Pho")
+  # Adding the family name
   dat$fam <- fam_name
+  # Return data frame for one family
   return(as.data.frame(dat))
 }
 
+# Call function to read all data frames into list
 data <- lapply(files, FUN = read_files)
+# Bind the data frames together to form one big data frame
 data <- do.call(rbind, data)
+# Show data
 data
 
-# Count number of families with a gene
-genes <- data %>% 
+# Count number of families with a variant in a specific gene
+# - First: summarise all variants within a family i.e. keep only one row for
+#   each gene with variants for each family
+genes <- data %>%
   group_by(fam, gene) %>%
-  summarise(HGVS_c = paste0(HGVS_c, collapse = ","), 
-            HGVS_p = paste0(HGVS_p, collapse = ","), 
+  summarise(HGVS_c = paste0(HGVS_c, collapse = ","),
+            HGVS_p = paste0(HGVS_p, collapse = ","),
             effect = paste0(effect, collapse = ","))
 
-res1 = genes %>% 
+# - Second: Count for each gene, the number of families with at least one
+#   variant in that gene, and sort the counts in descending order.
+res1 = genes %>%
   group_by(gene) %>%
-  summarise(families = paste0(fam, collapse = ","), 
+  summarise(families = paste0(fam, collapse = ","),
             count = n(),
             HGVS_c = paste0(HGVS_c, collapse = ", "),
             HGVS_p = paste0(HGVS_p, collapse = ", "),
             effect = paste0(effect, collapse = ", ")) %>%
   arrange(desc(count))
+
+# View the resulting data frame
 View(res1)
 
-# Count variants
-variants <- data %>% 
+# Count the number of times a unique variant is seen in the families
+# - First: summarise all unique variants within a family i.e. keep only one row
+#   for each unique variant for each family
+variants <- data %>%
   group_by(fam, gene) %>%
   count(HGVS_c, HGVS_p, seq_ontology, effect)
 
-res2 = variants %>% 
+# - Second: Count for each unique variant, the number of families with at least
+#   one variant in that gene, and sort the counts in descending order.
+res2 = variants %>%
   group_by(gene, HGVS_c, HGVS_p, seq_ontology, effect) %>%
   summarise(families = paste0(fam, collapse = ", "), count = n()) %>%
   arrange(desc(count))
-#arrange(gene,desc(count))
-View(res2)
 
+# View the resulting data frame
+View(res2)
